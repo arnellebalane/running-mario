1e82cd9411ca111a059cc949c7a9f02f3f3bfa6d
Arnelle Balane <arnellebalane@gmail.com>
2015-07-31 10:46:36 +0800

    added autoprefixer to prefix compiled css rules

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,14 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import jade from 'gulp-jade';
 import stylus from 'gulp-stylus';
+import autoprefixer from 'gulp-autoprefixer';
 
 
 gulp.task('styles', () => {
     return gulp.src('stylesheets/**/*.styl')
         .pipe(plumber())
         .pipe(stylus())
+        .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
         .pipe(gulp.dest('build/stylesheets/'));
 });
 
